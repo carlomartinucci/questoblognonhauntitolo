bcf6198bab8d16f8c660117d9e85ba9c684bc420
Carlo Martinucci <carlo.martinucci@gmail.com>
2020-11-15 15:01:22 +0100

    Add useCookie and style input subscription

---
+++ src/hooks/useCookie.js
@@ -0,0 +1,30 @@
+// Thanks to https://github.com/rrudol/useCookie
+
+import { useState } from "react"
+import Cookies from "js-cookie"
+
+/**
+ * useCookie - React Hook for Cookies based on js-cookie
+ * @param {string} key Cookie name
+ * @param {Object|string} [initialValue]  Value will be assign if cookie doesn't exist.
+ * @returns {Array} Returns cookie value, and the function to update it.
+ */
+const useCookie = (key, initialValue) => {
+  const [item, setInnerValue] = useState(() => {
+    return Cookies.get(key) || initialValue
+  })
+
+  /**
+   * Set value of cookie
+   * @param {Object|string} value
+   * @param {Cookies.CookieAttributes} [options]
+   */
+  const setValue = (value, options) => {
+    setInnerValue(value)
+    Cookies.set(key, value, options)
+  }
+
+  return [item, setValue]
+}
+
+export default useCookie
